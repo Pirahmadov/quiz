90ee942bf09c2acb146160f2f7eafa8a9b894c92
Pirahmadov <orkhan.pirahmadov@gmail.com>
2022-06-25 00:45:55 +0400

    quiz-app-finish

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const main = document.querySelector(".main");
-const next = document.querySelector(".next")
+const next = document.querySelector(".next");
+let idNumber = 1;
+let blocked = false;
+let trueAnswers = 0;
+let falseAnswers = 0;
+const res = document.querySelector(".result");
 
 const questions = [
   {
     question: "iki vur iki",
+    id: 1,
     answers: [
       { variant: "A", answer: "4", rightAnswer: true },
       { variant: "B", answer: "2", rightAnswer: false },
@@ -13,6 +19,27 @@ const questions = [
   },
   {
     question: "iki plus iki",
+    id: 2,
+    answers: [
+      { variant: "A", answer: "4", rightAnswer: true },
+      { variant: "B", answer: "2", rightAnswer: false },
+      { variant: "C", answer: "3", rightAnswer: false },
+      { variant: "D", answer: "5", rightAnswer: false },
+    ],
+  },
+  {
+    question: "uch plus iki",
+    id: 3,
+    answers: [
+      { variant: "A", answer: "4", rightAnswer: true },
+      { variant: "B", answer: "2", rightAnswer: false },
+      { variant: "C", answer: "3", rightAnswer: false },
+      { variant: "D", answer: "5", rightAnswer: false },
+    ],
+  },
+  {
+    question: "dord plus iki",
+    id: 4,
     answers: [
       { variant: "A", answer: "4", rightAnswer: true },
       { variant: "B", answer: "2", rightAnswer: false },
@@ -21,9 +48,26 @@ const questions = [
     ],
   },
 ];
-let blocked = false
+const questionsAmmount = questions.length;
+const nextQuestion = () => {
+  if (idNumber !== questionsAmmount && blocked) {
+    idNumber++;
+    getQuestions();
+    blocked = false;
+  } else if (blocked) {
+    next.innerHTML = "finish";
+    blocked = false;
+  } else if (idNumber === questionsAmmount && !blocked) {
+    res.innerHTML = `Duz: ${trueAnswers} Sehv: ${falseAnswers}`;
+  }
+};
+
 const getQuestions = () => {
-  questions.map((e) => {
+  let availableQuestion = questions.filter((element) => {
+    return element.id === idNumber;
+  });
+  main.innerHTML = "";
+  availableQuestion.map((e) => {
     const questionBox = document.createElement("div");
 
     questionBox.innerHTML = e.question;
@@ -32,18 +76,18 @@ const getQuestions = () => {
     e.answers.map((ans) => {
       const li = document.createElement("li");
       const variant = document.createElement("span");
-      variant.classList.add("variant")
+      variant.classList.add("variant");
       variant.addEventListener("click", () => {
-        if(blocked) return
+        if (blocked) return;
         if (ans.rightAnswer) {
           answer.classList.add("active");
+          trueAnswers++;
           blocked = true;
-          
         } else {
           answer.classList.add("error");
-          blocked = true
+          falseAnswers++;
+          blocked = true;
         }
-        
       });
       variant.innerHTML = ans.variant;
 
@@ -58,3 +102,5 @@ const getQuestions = () => {
   });
 };
 getQuestions();
+
+next.addEventListener("click", nextQuestion);
